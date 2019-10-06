0eae1abfe81900fa0b97355171055b1699bf4526
Yusuke Ogata <yusuke.ogata.yo@gmail.com>
2019-10-06 17:45:18 +0900

    add js/main1.js

---
+++ js/main1.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+
+  let show = document.getElementById('show');
+  let main = document.getElementById('main');
+  
+  show.addEventListener('click', function() {
+    main.className = main.className === 'menu-open' ? '':  'menu-open';
+  });
+  
+}
